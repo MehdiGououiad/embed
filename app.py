1c1ec724ee3bd701c59066e5d1c0b1b05ee52336
mehdigououiad <mehdigououiad@gmail.com>
2024-01-04 17:02:23 +0100

    emnbed

--- app.py
+++ app.py
@@ -20,21 +20,22 @@ def save_embeddings(file_path, embeddings):
 def load_embeddings(file_path):
     return np.load(file_path, allow_pickle=True)
 
-# Load the JSON file
-with open('combined_avantages_absences_by_intent.json', 'r', encoding='utf-8') as file:
+# Load the JSON data
+with open('intents_questions_responses_renamed.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
-
 # File paths
 embeddings_file = 'embeddings.npy'
 
 # Extract sentences and intents
 sentences = []
 intents = []
-for parent_intent, child_intents in data.items():
-    for child_intent, details in child_intents.items():
-        for sentence in details['Questions']:
-            sentences.append(sentence)
-            intents.append(child_intent)
+
+# Iterate over the data to extract sentences and intents
+for parent_intent, content in data.items():
+    for question in content['Questions']:
+        sentences.append(question)
+        intents.append(parent_intent)
+
 
 # Load or compute embeddings
 if embeddings_file_exists(embeddings_file):
@@ -56,7 +57,6 @@ def find_most_similar_intents(input_sentence, num_intents=3):
 
     # Encode the new sentence
     new_embedding = model.encode([input_sentence])
-
     # Flatten the embedding if necessary
     if len(new_embedding.shape) == 3:
         new_embedding = new_embedding.squeeze()
@@ -72,7 +72,6 @@ def find_most_similar_intents(input_sentence, num_intents=3):
     for i in sorted_indices:
         intent = intents[i]
         confidence = float(similarity_scores[0][i])
-        
         if intent not in unique_intents:
             top_intents.append({"intent": intent, "confidence": confidence})
             unique_intents.add(intent)
